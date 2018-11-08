e65afc64764d138541467c13ac5f4806e2bbb429
Adam Saland <a.jpagnano@gmail.com>
2018-11-08 22:05:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,15 +1,35 @@
 def instantiate_new_array
+  []
 end
 
 def array_with_two_elements
+  Array.new(2)
 end
 
 def first_element
 end
 
-first_element
-third_element
-last_element
-first_element_with_array_methods
-last_element_with_array_methods
-length_of_array
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
+
+def length_of_array
+  
+end
